932482252b90ae8c5370e90ac328fc19cd448e72
BrianJenney <brianjenney83@gmail.com>
2017-09-24 13:48:21 -0700

    remove es6 and cors stuff for cdn

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 
-const questions = [
+var questions = [
   "What is the median age of our family? What is the mode age?",
   "If there are 100 windows on ONE side of a building and each one takes about 5 minutes to wash, how long (in minutes) would it take to wash every window? How long would it take 2 men, working 8 hours a day with a one hour lunch break?","An anagram is a word, that when spelled differently, makes another word (ex: iceman / cinema). What are 10 more anagrams you can think of?", "I estimate I write about 100 lines of code a day. Excluding weekends, how many lines of code have I written in a year? There are about 46 lines on a full page. How many pages of code have I written in a year?", "How would you explain to Ava the concept of multiplication?", "A man says he drives 100,000 miles a year because of his job as a traveling salesman. Let's say he drives about 20 miles before lunch and another 15 after lunch, then another 25 to get home. He works 5 days a week. Is his claim true?",
   "Estimate how much time, in days you have spent in the bathroom?",
@@ -14,7 +14,7 @@ const questions = [
 
 $("button").click(function(){
 
-  const rando = Math.floor(Math.random() * questions.length);
+  var rando = Math.floor(Math.random() * questions.length);
 
   questions.slice(rando,1);
 
